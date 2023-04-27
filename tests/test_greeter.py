2434b42994290398a75836566b47381b3c0c1d58
Paolo Laurenti <laurentipaolo@gmail.com>
2023-04-27 15:18:02 +0200

    add test to check that no greeting is sent when today is  no friends birthday

--- tests/test_greeter.py
+++ tests/test_greeter.py
@@ -31,7 +31,9 @@ class TestGreeter:
         clock_double = ClockTestDouble()
         clock_double.stub_local_tz_today(date(2023, 4, 22))
         greetings_notifier_double = GreetingsNotifierTestDouble()
-        greeter = Greeter(friends_gateway_double, clock_double, greetings_notifier_double)
+        greeter = Greeter(
+            friends_gateway_double, clock_double, greetings_notifier_double
+        )
 
         greeter.send_greetings()
 
@@ -49,3 +51,30 @@ class TestGreeter:
         assert len(notified_greetings) == 2
         assert notified_greetings[0] == expected_greeting_1
         assert notified_greetings[1] == expected_greeting_2
+
+    def test_no_greets_due_to_no_friends_birthday(self):
+        friends_gateway_double = FriendsGatewayTestDouble()
+        stubbed_friends = [
+            Friend(
+                name="Franco Franchi",
+                email="franco@franchi.com",
+                birthday=date(1974, 4, 22),
+            ),
+            Friend(
+                name="Marco Marchi",
+                email="marco@marchi.com",
+                birthday=date(1980, 5, 11),
+            )
+        ]
+        friends_gateway_double.stub_friends(stubbed_friends)
+        clock_double = ClockTestDouble()
+        clock_double.stub_local_tz_today(date(2023, 1, 1))
+        greetings_notifier_double = GreetingsNotifierTestDouble()
+        greeter = Greeter(
+            friends_gateway_double, clock_double, greetings_notifier_double
+        )
+
+        greeter.send_greetings()
+
+        notified_greetings = greetings_notifier_double.get_spied_notified_greetings()
+        assert len(notified_greetings) == 0
