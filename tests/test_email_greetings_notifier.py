9d7f18214588064f3f5fca5e38e417c847b1d5be
Paolo Laurenti <laurentipaolo@gmail.com>
2023-04-28 15:16:59 +0200

    add integration test to verify that we are able to send many greetings via email

--- tests/test_email_greetings_notifier.py
+++ tests/test_email_greetings_notifier.py
@@ -6,25 +6,34 @@ from tests.support.fake_mailer import FakeMailer
 
 
 class TestEmailGreetingsNotifier:
-    def test_greet_one_friend(self):
+    def test_greet_many_friends(self):
         mailer = FakeMailer()
-        notifier = EmailGreetingsNotifier(
-            email_from="greeter@kata.com", mailer=mailer
-        )
+        notifier = EmailGreetingsNotifier(email_from="greeter@kata.com", mailer=mailer)
         greetings = [
             Greeting(
                 name="Franco Franchi",
                 email="franco@franchi.com",
                 birthday=date(1974, 4, 22),
-            )
+            ),
+            Greeting(
+                name="Luciana Luciani",
+                email="luciana@luciani.com",
+                birthday=date(1980, 4, 22),
+            ),
         ]
         notifier.notify(greetings)
 
         emails = mailer.get_sent_emails()
-        assert len(emails) == 1
+        assert len(emails) == 2
         assert emails[0] == Email(
             from_address="greeter@kata.com",
             to_address="franco@franchi.com",
             subject="Happy birthday Franco Franchi!",
             text_body="Dear Franco Franchi, happy birthday.",
+        )
+        assert emails[1] == Email(
+            from_address="greeter@kata.com",
+            to_address="luciana@luciani.com",
+            subject="Happy birthday Luciana Luciani!",
+            text_body="Dear Luciana Luciani, happy birthday.",
         )
